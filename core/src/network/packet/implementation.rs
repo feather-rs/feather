9895796c85bffe012042ac23fef099313c7df0aa
caelunshun <caelum12321@gmail.com>
2019-06-13 12:32:13 -0600

    Implement all status, handshake, and login packets

--- core/src/network/packet/implementation.rs
+++ core/src/network/packet/implementation.rs
@@ -13,6 +13,8 @@ lazy_static! {
     };
 }
 
+// SERVERBOUND
+
 #[derive(Default)]
 pub struct Handshake {
     pub protocol_version: u32,
@@ -108,5 +110,144 @@ impl Packet for EncryptionResponse {
 
     fn write_to(&self, buf: &mut ByteBuf) {
         unimplemented!()
+    }
+}
+
+#[derive(Default)]
+pub struct Request {
+
+}
+
+impl Packet for Request {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        Some(())
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        unimplemented!()
+    }
+}
+
+#[derive(Default)]
+pub struct Ping {
+    payload: u64,
+}
+
+impl Packet for Ping {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        self.payload = buf.read_u64();
+        Some(())
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        unimplemented!()
+    }
+}
+
+// CLIENTBOUND
+#[derive(Default)]
+pub struct DisconnectLogin {
+    reason: String,
+}
+
+impl Packet for DisconnectLogin {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        self.reason = buf.read_string()?;
+        Some(())
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        buf.write_string(self.reason.as_str());
+    }
+}
+
+#[derive(Default)]
+pub struct EncryptionRequest {
+    server_id: String,
+    public_key_len: i32,
+    public_key: Vec<u8>,
+    verify_token_len: i32,
+    verify_token: Vec<u8>,
+}
+
+impl Packet for EncryptionRequest {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        unimplemented!()
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        buf.write_string(self.server_id.as_str());
+        buf.write_var_int(self.public_key_len);
+
+        for val in self.public_key {
+            buf.write_u8(val);
+        }
+
+        buf.write_var_int(self.verify_token_len);
+        for val in self.verify_token {
+            buf.write_u8(val);
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct LoginSuccess {
+    uuid: String,
+    username: String,
+}
+
+impl Packet for LoginSuccess {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        unimplemented!()
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        buf.write_string(self.uuid.as_str());
+        buf.write_string(self.username.as_str());
+    }
+}
+
+#[derive(Default)]
+pub struct SetCompression {
+    threshold: i32,
+}
+
+impl Packet for SetCompression {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        unimplemented!()
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        buf.write_var_int(self.threshold);
+    }
+}
+
+#[derive(Default)]
+pub struct Response {
+    json_response: String,
+}
+
+impl Packet for Response {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        unimplemented!()
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        buf.write_string(self.json_response.as_str());
+    }
+}
+
+#[derive(Default)]
+pub struct Pong {
+    payload: u64,
+}
+
+impl Packet for Pong {
+    fn read_from(&mut self, buf: &mut ByteBuf) -> Option<()> {
+        unimplemented!()
+    }
+
+    fn write_to(&self, buf: &mut ByteBuf) {
+        buf.write_u64(self.payload);
     }
 }
