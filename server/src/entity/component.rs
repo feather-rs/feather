f83d5f8ca49282589426807da677a23a7ba515a0
caelunshun <caelum12321@gmail.com>
2019-08-11 16:36:49 -0600

    Split `entity` module into separate files

---
+++ server/src/entity/component.rs
@@ -0,0 +1,27 @@
+//! Various Specs components.
+
+use feather_core::world::Position;
+use feather_core::Gamemode;
+use specs::storage::BTreeStorage;
+use specs::{Component, VecStorage};
+use uuid::Uuid;
+
+pub struct PlayerComponent {
+    pub profile_properties: Vec<mojang_api::ServerAuthProperty>,
+    pub gamemode: Gamemode,
+}
+
+impl Component for PlayerComponent {
+    type Storage = BTreeStorage<Self>;
+}
+
+pub struct EntityComponent {
+    pub uuid: Uuid,
+    pub display_name: String,
+    pub position: Position,
+    pub on_ground: bool,
+}
+
+impl Component for EntityComponent {
+    type Storage = VecStorage<Self>;
+}
