7ac169add8675b068a4064dd4fb8721da152a8c7
momothereal <momothereal.mc@gmail.com>
2019-08-29 23:39:22 -0400

    Add tests for player data loading

--- core/src/save/player_data.rs
+++ core/src/save/player_data.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 
+use std::io::Read;
 use uuid::Uuid;
 
 /// Represents the contents of a player data file.
@@ -9,8 +10,30 @@ pub struct PlayerData {
     pub gamemode: Option<i32>,
 }
 
+fn load_from_file<R: Read>(reader: R) -> Result<PlayerData, nbt::Error> {
+    nbt::from_gzip_reader::<_, PlayerData>(reader)
+}
+
 pub fn load_player_data(uuid: Uuid) -> Result<PlayerData, nbt::Error> {
     let file = File::open(format!("world/playerdata/{}.dat", uuid))?;
-    let data = nbt::from_gzip_reader::<_, PlayerData>(file)?;
+    let data = load_from_file(file)?;
     Ok(data)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Gamemode;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_deserialize_player() {
+        let cursor = Cursor::new(include_bytes!("player.dat").to_vec());
+
+        let player = load_from_file(cursor).unwrap();
+        assert_eq!(
+            player.gamemode.unwrap(),
+            i32::from(Gamemode::Creative.get_id())
+        );
+    }
 }
