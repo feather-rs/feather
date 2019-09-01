90f0254519f5f7c7899d7d1fae2670f2b46962e3
caelunshun <caelum12321@gmail.com>
2019-09-01 09:43:53 -0600

    Add Util::broadcast() function
    This function adds an easy way to broadcast a packet
    to all players who are able to see a given chunk.
    For example, movement packets for a player can
    be sent to all players able to see that player's
    chunk.
    
    The function hasn't yet been used, but player action
    broadcast systems will soon be modified to use
    `Util::broadcast()`.
    
    Also see #78.

--- server/src/util/mod.rs
+++ server/src/util/mod.rs
@@ -1,6 +1,6 @@
 //! Assorted utilities for use in Feather's codebase.
 use bumpalo::Bump;
-use feather_core::{ItemStack, Position};
+use feather_core::{ChunkPosition, ItemStack, Packet, Position};
 use glm::DVec3;
 use spawn::Spawner;
 use thread_local::ThreadLocal;
@@ -9,8 +9,12 @@ use thread_local::ThreadLocal;
 mod macros;
 mod spawn;
 
+use crate::chunk_logic::ChunkHolders;
+use crate::network::{send_packet_to_player, NetworkComponent};
 pub use macros::*;
 pub use spawn::SpawnerSystem;
+use specs::storage::GenericReadStorage;
+use specs::Entity;
 
 /// Converts float-based velocity in blocks per tick
 /// to the obnoxious format used by the protocol.
@@ -42,6 +46,9 @@ pub fn protocol_velocity(vel: DVec3) -> (i16, i16, i16) {
 /// needs. Note, however, that the entity isn't created
 /// until the handling dispatcher stage. These functions simply
 /// redirect to `entity::Spawner`.
+/// * `broadcast` - broadcasts a packet to all players
+/// who are able to see a given chunk. This can be used
+/// to broadcast movement updates, for example.
 #[derive(Debug, Default)]
 pub struct Util {
     /// Thread-local bump allocator, reset
@@ -81,5 +88,92 @@ impl Util {
         for bump in self.bump.iter_mut() {
             bump.reset();
         }
+    }
+
+    /// Broadcasts a packet to all players who
+    /// are able to see a given chunk.
+    ///
+    /// The packet is sent instantly, not lazily,
+    /// unlike many of the `Util` functions.
+    ///
+    /// If `neq` is set to an entity, the packet
+    /// will not be sent to that player.
+    ///
+    /// This function runs in linear time with
+    /// regard to the number of players able to see
+    /// the chunk.
+    pub fn broadcast<P, N>(
+        &self,
+        chunk_holders: &ChunkHolders,
+        networks: &N,
+        chunk: ChunkPosition,
+        packet: P,
+        neq: Option<Entity>,
+    ) where
+        P: Packet + Clone + 'static,
+        N: GenericReadStorage<Component = NetworkComponent>,
+    {
+        // Iterate over entities in the chunk_holders
+        // entry for the chunk. If they are a player,
+        // send the packet.
+        // This works because any player able to see
+        // a chunk will always have a chunk holder on the chunk.
+        if let Some(holders) = chunk_holders.holders_for(chunk) {
+            for entity in holders {
+                if let Some(neq) = neq.as_ref() {
+                    if *neq == *entity {
+                        continue;
+                    }
+                }
+
+                // If the entity doesn't have a network component,
+                // skip it.
+                if let Some(network) = networks.get(*entity) {
+                    send_packet_to_player(&network, packet.clone());
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::testframework as t;
+    use feather_core::network::packet::implementation::EntityHeadLook;
+    use feather_core::PacketType;
+    use specs::WorldExt;
+
+    #[test]
+    fn test_broadcast() {
+        let mut chunk_holders = ChunkHolders::default();
+
+        let chunk = ChunkPosition::new(0, 0);
+        let other_chunk = ChunkPosition::new(10, 1);
+
+        let (mut world, _) = t::builder().build();
+        let player1 = t::add_player(&mut world);
+        let player2 = t::add_player(&mut world);
+        let player3 = t::add_player(&mut world);
+
+        chunk_holders.insert_holder(chunk, player1.entity);
+        chunk_holders.insert_holder(chunk, player2.entity);
+        chunk_holders.insert_holder(other_chunk, player3.entity);
+
+        let packet = EntityHeadLook::default();
+
+        let util = Util::default();
+
+        util.broadcast(
+            &chunk_holders,
+            &world.read_component(),
+            chunk,
+            packet,
+            Some(player2.entity),
+        );
+
+        t::assert_packet_received(&player1, PacketType::EntityHeadLook);
+        t::assert_packet_not_received(&player2, PacketType::EntityHeadLook);
+        t::assert_packet_not_received(&player3, PacketType::EntityHeadLook);
     }
 }
