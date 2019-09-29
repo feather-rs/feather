0a41bb43a31b9944a54e603513a0a07761c98e89
caelunshun <caelum12321@gmail.com>
2019-09-29 11:00:33 -0600

    Rename serialize.rs -> codec.rs

--- core/src/network/mod.rs
+++ core/src/network/mod.rs
@@ -1,6 +1,6 @@
+pub mod codec;
 pub mod mctypes;
 pub mod packet;
-pub mod serialize;
 
 pub fn cast_packet<P: packet::Packet + 'static + Send>(packet: &dyn packet::Packet) -> &P {
     packet.as_any().downcast_ref().unwrap()
