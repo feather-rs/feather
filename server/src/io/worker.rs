4affa5ffad2f69a231a69c4edec30e5a29b61352
Thijs Raymakers <thijs@raymakers.nl>
2019-10-16 12:18:19 +0200

    Rename NoneError to OptionIsNone

--- server/src/io/worker.rs
+++ server/src/io/worker.rs
@@ -26,7 +26,7 @@ enum Error {
     #[fail(display = "Option that should not be None was None")]
     /// An Error type than can be used as the error type of using the Try operator on Option
     /// types. In rust-core, this is an unstable feature (issue #42327)
-    NoneError,
+    OptionIsNone,
 }
 
 /// Runs a worker task for the given client.
@@ -129,7 +129,7 @@ async fn _run_worker(
                                     Action::JoinGame(res) => {
                                         let info = NewClientInfo {
                                             ip,
-                                            username: res.username.ok_or(Error::NoneError)?,
+                                            username: res.username.ok_or(Error::OptionIsNone)?,
                                             profile: res.props,
                                             uuid: res.uuid,
                                             sender: tx_server_to_worker.clone(),
