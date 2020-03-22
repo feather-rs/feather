58d25f3d68967e5412e73f9712816a6f2ae55aaa
Redrield <redrield@gmail.com>
2020-03-22 16:17:41 -0400

    Add support for shooting arrows (#185)
    > Physics for the entity is still a bit weird once it's spawned, however, this code successfully handles spawning the entity when the bow is released.

--- server/src/systems.rs
+++ server/src/systems.rs
@@ -13,6 +13,7 @@ pub fn build_executor() -> Executor {
         .with(packet_handlers::handle_held_item_change)
         .with(packet_handlers::handle_animation)
         .with(packet_handlers::handle_player_block_placement)
+        .with(packet_handlers::handle_player_use_item)
         .with(packet_handlers::handle_player_digging)
         .with(packet_handlers::handle_chat)
         .with(weather::handle_weather)
