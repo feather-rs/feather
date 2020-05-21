0bc9e3ab6b34e0469238a19edbf3f5c6a6df3eb1
Amber Kowalski <amberkowalski03@gmail.com>
2020-05-21 01:39:26 -0500

    Split block placement and interaction from the packet handler

--- server/player/src/packet_handlers/placement.rs
+++ server/player/src/packet_handlers/placement.rs
@@ -9,7 +9,7 @@ use feather_core::items::ItemStack;
 use feather_core::network::packets::PlayerBlockPlacement;
 use feather_core::util::Gamemode;
 use feather_server_types::{BlockUpdateCause, Game, HeldItem, InventoryUpdateEvent, PacketBuffers};
-use fecs::World;
+use fecs::{Entity, World};
 use smallvec::smallvec;
 use std::sync::Arc;
 
@@ -24,66 +24,102 @@ pub fn handle_player_block_placement(
     packet_buffers
         .received::<PlayerBlockPlacement>()
         .for_each_valid(world, |world, (player, packet)| {
-            // TODO: handle slabs, blocks with directions, etc.
-            let gamemode = *world.get::<Gamemode>(player);
-            let inventory = world.get::<Inventory>(player);
-
-            let item = match inventory.item_in_main_hand(player, world) {
-                Some(item) => item,
-                None => return, // No block to place
-            };
-
-            drop(inventory);
-
-            let block = match item.ty.to_block() {
-                Some(block) => block,
-                None => return, // Item is not a block
-            };
-
-            let placed_on = match game.block_at(packet.location) {
+            let target_block = match game.block_at(packet.location) {
                 Some(block) => block,
                 None => {
-                    game.disconnect(player, world, "attempted to place block in unloaded chunk");
-                    return;
-                }
-            };
-
-            // TODO: waterlogged blocks, more
-            let pos = match placed_on.kind() {
-                BlockKind::Grass | BlockKind::TallGrass | BlockKind::Water | BlockKind::Lava => {
-                    packet.location
-                }
-                _ => packet.location + packet.face.placement_offset(),
-            };
-
-            game.set_block_at(world, pos, block, BlockUpdateCause::Entity(player));
-
-            let held_item = world.get::<HeldItem>(player).0;
-            let inventory = world.get::<Inventory>(player);
-
-            // Update player's inventory if in survival
-            if gamemode == Gamemode::Survival {
-                if item.amount == 0 {
-                    drop(inventory);
                     game.disconnect(
                         player,
                         world,
-                        "attempted to place block with zero-sized item stack",
+                        "Attempted to interact with block in unloaded chunk",
                     );
                     return;
                 }
+            };
 
-                let item = ItemStack::new(item.ty, item.amount - 1);
-                inventory
-                    .set_item_at(Area::Hotbar, held_item, item)
-                    .unwrap();
-
-                let event = InventoryUpdateEvent {
-                    slots: smallvec![slot(Area::Hotbar, held_item)],
-                    player,
-                };
-                drop(inventory);
-                game.handle(world, event);
-            }
+            // Decide whether the player should place a block or interact with the block they are targeting
+            // TODO: Maybe player shifting may need to be taken into account (shift click on interactable block)
+            match target_block.kind() {
+                // Case which handles blocks that can be interacted with
+                BlockKind::Furnace | BlockKind::CraftingTable => {
+                    handle_block_interaction(game, world, player, target_block.kind(), packet);
+                }
+                // Case which handles block placement (ie: target block is not interactable)
+                _ => {
+                    handle_block_placement(game, world, player, target_block.kind(), packet);
+                }
+            };
         });
+}
+
+pub fn handle_block_placement(
+    game: &mut Game,
+    world: &mut World,
+    player: Entity,
+    target_block_kind: BlockKind,
+    packet: PlayerBlockPlacement,
+) {
+    let gamemode = *world.get::<Gamemode>(player);
+
+    let inventory = world.get::<Inventory>(player);
+
+    let item = match inventory.item_in_main_hand(player, world) {
+        Some(item) => item,
+        None => return, // No block to place
+    };
+
+    drop(inventory);
+
+    let block = match item.ty.to_block() {
+        Some(block) => block,
+        None => return, // Item is not a block
+    };
+
+    // TODO: waterlogged blocks, more
+    let pos = match target_block_kind {
+        BlockKind::Grass | BlockKind::TallGrass | BlockKind::Water | BlockKind::Lava => {
+            packet.location
+        }
+        _ => packet.location + packet.face.placement_offset(),
+    };
+
+    game.set_block_at(world, pos, block, BlockUpdateCause::Entity(player));
+
+    let held_item = world.get::<HeldItem>(player).0;
+    let inventory = world.get::<Inventory>(player);
+
+    // Update player's inventory if in survival
+    if gamemode == Gamemode::Survival {
+        if item.amount == 0 {
+            drop(inventory);
+            game.disconnect(
+                player,
+                world,
+                "attempted to place block with zero-sized item stack",
+            );
+            return;
+        }
+
+        let item = ItemStack::new(item.ty, item.amount - 1);
+        inventory
+            .set_item_at(Area::Hotbar, held_item, item)
+            .unwrap();
+
+        let event = InventoryUpdateEvent {
+            slots: smallvec![slot(Area::Hotbar, held_item)],
+            player,
+        };
+        drop(inventory);
+        game.handle(world, event);
+    }
+}
+
+/// Handles interaction with blocks that have an inventory
+pub fn handle_block_interaction(
+    game: &mut Game,
+    world: &mut World,
+    player: Entity,
+    target_block_kind: BlockKind,
+    packet: PlayerBlockPlacement,
+) {
+    let gamemode = *world.get::<Gamemode>(player);
 }
