bf94ca24a25b102f72e051795c725e16ba8f6884
Oliver Nordbjerg <hi@notbjerg.me>
2020-05-22 15:55:44 +0200

    Improve readability

--- server/commands/src/impls.rs
+++ server/commands/src/impls.rs
@@ -215,13 +215,12 @@ pub fn say(ctx: &mut CommandCtx, message: TextArgument) -> anyhow::Result<()> {
 }
 
 #[command(usage = "me <action>")]
-pub fn me(ctx: &mut CommandCtx, action: TextArgument) -> anyhow::Result<()> {
-    let name = ctx.world.try_get::<Name>(ctx.sender);
-
-    let sender_name = if let Some(name) = &name { &name.0 } else { "@" };
-    let command_output = Text::from(format!("* {} {}", sender_name, action.0));
-
-    drop(name);
+pub fn me(ctx: &mut CommandCtx, TextArgument(action): TextArgument) -> anyhow::Result<()> {
+    let command_output = {
+        let name = ctx.world.try_get::<Name>(ctx.sender);
+        let sender_name = name.as_ref().map_or("@", |Name(n)| n);
+        Text::from(format!("* {} {}", sender_name, action));
+    };
 
     ctx.game.handle(
         &mut ctx.world,
