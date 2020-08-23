aa3cb5a9c5fe2a68dcadbd1245e07d1b042969dc
caelunshun <caelunshun@gmail.com>
2020-08-23 01:42:02 -0600

    ecs: Change to be a much thinner wrapper over hecs
    I switched from Legion to hecs because:
    * Legion's API is generally verbose and difficult to use in cases it wasn't
    built for.
    * Legion brings on unnecessary functionality, such as its systems scheduler, which is
    bad impact on compile times.
    * Legion doesn't support proper change detection. (Hecs doesn't either,
    but right now I'm using Bevy's fork of it, which has change detection
    in first-class.)

--- crates/ecs/tests/random_access.rs
+++ crates/ecs/tests/random_access.rs
@@ -1,13 +1,15 @@
-use feather_ecs::{ComponentError, Ecs, EntityBuilder};
+use feather_ecs::{Ecs, EntityBuilder, ComponentError};
 
 #[test]
 fn add_simple_entity() {
     let mut ecs = Ecs::new();
-    let entity = EntityBuilder::new()
-        .with(10i32)
-        .with(15u32)
-        .with(usize::MAX)
-        .spawn_into(&mut ecs);
+    let entity = ecs.spawn(
+        EntityBuilder::new()
+            .add(10i32)
+            .add(15u32)
+            .add(usize::MAX)
+            .build(),
+    );
 
     assert_eq!(*ecs.get::<i32>(entity).unwrap(), 10);
     assert_eq!(*ecs.get::<u32>(entity).unwrap(), 15);
@@ -20,7 +22,7 @@ fn add_simple_entity() {
 #[test]
 fn add_remove_entities() {
     let mut ecs = Ecs::new();
-    let entity = EntityBuilder::new().with("test").spawn_into(&mut ecs);
+    let entity = ecs.spawn(EntityBuilder::new().add("test").build());
 
     assert_eq!(*ecs.get::<&'static str>(entity).unwrap(), "test");
 
@@ -28,39 +30,16 @@ fn add_remove_entities() {
 
     assert!(matches!(
         ecs.get::<&'static str>(entity).err(),
-        Some(ComponentError::InvalidEntity(_))
+        Some(ComponentError::NoSuchEntity)
     ));
     assert!(ecs.despawn(entity).is_err());
 }
 
 #[test]
-fn double_mutable_borrow() {
-    let mut ecs = Ecs::new();
-    let entity = EntityBuilder::new().with(10i32).spawn_into(&mut ecs);
-
-    let ref1 = ecs.get_mut::<i32>(entity).unwrap();
-
-    assert!(matches!(
-        ecs.get_mut::<i32>(entity),
-        Err(ComponentError::Borrowed(_))
-    ));
-    assert!(matches!(
-        ecs.get::<i32>(entity),
-        Err(ComponentError::MutablyBorrowed(_))
-    ));
-
-    drop(ref1);
-    assert!(ecs.get::<i32>(entity).is_ok());
-}
-
-#[test]
 fn fine_grained_borrow_checking() {
     let mut ecs = Ecs::new();
-    let entity1 = EntityBuilder::new()
-        .with(())
-        .with(16i32)
-        .spawn_into(&mut ecs);
-    let entity2 = EntityBuilder::new().with(14i32).spawn_into(&mut ecs);
+    let entity1 = ecs.spawn(EntityBuilder::new().add(()).add(16i32).build());
+    let entity2 = ecs.spawn(EntityBuilder::new().add(14i32).build());
 
     let mut e1 = ecs.get_mut::<i32>(entity1).unwrap();
     let e2 = ecs.get_mut::<i32>(entity2).unwrap();
@@ -70,9 +49,6 @@ fn fine_grained_borrow_checking() {
 
     *e1 = 12;
     assert_eq!(*e1, 12);
-
-    assert!(ecs.get_mut::<i32>(entity1).is_err());
-    assert!(ecs.get::<i32>(entity1).is_err());
 
     drop(e1);
     assert_eq!(*ecs.get_mut::<i32>(entity1).unwrap(), 12);
