0e5d171dbe917fbd38e50b520e4cb89e9341790a
PauMAVA <36012050+PauMAVA@users.noreply.github.com>
2021-02-20 19:10:57 +0100

    libcraft: Add GameRules monolithic struct. (#4)
    * Add GameRules monolithic struct.
    
    * Fixed file name and removed unecessary comments.
    
    * Added missing pub use.
    
    * Format code.

--- libcraft/crates/core/src/lib.rs
+++ libcraft/crates/core/src/lib.rs
@@ -4,11 +4,13 @@ pub mod block;
 mod consts;
 mod enchantment;
 mod gamemode;
+mod gamerules;
 mod positions;
 
 pub use consts::*;
 pub use enchantment::{Enchantment, EnchantmentKind};
 pub use gamemode::Gamemode;
+pub use gamerules::GameRules;
 pub use positions::{
     vec3, Aabb, BlockPosition, ChunkPosition, Mat4f, Position, Vec2d, Vec2f, Vec2i, Vec3d, Vec3f,
     Vec3i, Vec4d, Vec4f, Vec4i,
