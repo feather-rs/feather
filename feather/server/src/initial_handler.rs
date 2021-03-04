01c276a0c6b614f661310b5df9cfcb397621ccd4
UnrealCodec <43702198+FussballAndy@users.noreply.github.com>
2021-03-04 16:27:57 +0100

    Fixed server crash when joining with 1.8. (#377)
    * Fixed server crash when joining with 1.8 (tested)
    
    * Only cancelling if next state is Login

--- feather/server/src/initial_handler.rs
+++ feather/server/src/initial_handler.rs
@@ -11,7 +11,7 @@ use protocol::{
     codec::CryptKey,
     packets::{
         client::{HandshakeState, Ping},
-        server::{EncryptionRequest, LoginSuccess, Pong, Response},
+        server::{DisconnectLogin, EncryptionRequest, LoginSuccess, Pong, Response},
     },
     ClientHandshakePacket, ClientLoginPacket, ClientPlayPacket, ClientStatusPacket,
     ServerLoginPacket, ServerPlayPacket, ServerStatusPacket,
@@ -61,6 +61,18 @@ pub async fn handle(worker: &mut Worker) -> anyhow::Result<InitialHandling> {
     match handshake.next_state {
         HandshakeState::Status => handle_status(worker).await,
         HandshakeState::Login => {
+            if handshake.protocol_version < PROTOCOL_VERSION {
+                worker
+                    .write(ServerLoginPacket::DisconnectLogin(DisconnectLogin {
+                        reason: Text::from(
+                            "Invalid protocol! The server is running on version 1.16!",
+                        )
+                        .to_string(),
+                    }))
+                    .await
+                    .ok();
+                return Ok(InitialHandling::Disconnect);
+            }
             let proxy_data =
                 if let Some(crate::options::ProxyMode::Bungeecord) = worker.options().proxy_mode {
                     Some(proxy::do_bungee_ip_forwarding(&handshake)?)
