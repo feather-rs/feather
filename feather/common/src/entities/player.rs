bb33f009a67de680ba475febd707dca4715f4a66
Matthew McCune <matt@mattrick.org>
2021-03-04 16:18:12 -0800

    Handle held item change packet (#361)
    This adds basic handling to held item change packets. Fixes #355.
    
    This can be used in the future to add "get item in hand" support.

--- feather/common/src/entities/player.rs
+++ feather/common/src/entities/player.rs
@@ -1,8 +1,36 @@
+use anyhow::bail;
 use base::EntityKind;
-use ecs::EntityBuilder;
+use ecs::{EntityBuilder, SysResult};
 use quill_common::entities::Player;
 
 pub fn build_default(builder: &mut EntityBuilder) {
     super::build_default(builder);
     builder.add(Player).add(EntityKind::Player);
+}
+
+/// The hotbar slot a player's cursor is currently on
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct HotbarSlot(usize);
+
+impl HotbarSlot {
+    pub fn new(id: usize) -> Self {
+        Self(id)
+    }
+
+    pub fn default() -> Self {
+        Self(0)
+    }
+
+    pub fn get(&self) -> usize {
+        self.0
+    }
+
+    pub fn set(&mut self, id: usize) -> SysResult {
+        if id > 8 {
+            bail!("invalid hotbar slot id");
+        }
+
+        self.0 = id;
+        Ok(())
+    }
 }
--- feather/server/src/packet_handlers/interaction.rs
+++ feather/server/src/packet_handlers/interaction.rs
@@ -1,14 +1,17 @@
 use crate::{ClientId, NetworkId, Server};
+use common::entities::player::HotbarSlot;
+use common::events::InteractEntityEvent;
 use common::Game;
 use common::{
-    events::{BlockInteractEvent, BlockPlacementEvent, InteractEntityEvent},
+    events::{BlockInteractEvent, BlockPlacementEvent},
     interactable::InteractableRegistry,
 };
-use ecs::{Entity, SysResult};
-use libcraft_core::{BlockFace as LibcraftBlockFace, Hand, InteractionType, Vec3f};
+use ecs::{Entity, EntityRef, SysResult};
+use libcraft_core::{BlockFace as LibcraftBlockFace, Hand};
+use libcraft_core::{InteractionType, Vec3f};
 use protocol::packets::client::{
-    BlockFace, InteractEntity, InteractEntityKind, PlayerBlockPlacement, PlayerDigging,
-    PlayerDiggingStatus,
+    BlockFace, HeldItemChange, InteractEntity, InteractEntityKind, PlayerBlockPlacement,
+    PlayerDigging, PlayerDiggingStatus,
 };
 
 /// Handles the player block placement packet. Currently just removes the block client side for the player.
@@ -192,4 +195,38 @@ pub fn handle_interact_entity(
     game.ecs.insert_entity_event(player, event)?;
 
     Ok(())
+}
+
+pub fn handle_held_item_change(player: EntityRef, packet: HeldItemChange) -> SysResult {
+    let new_id = packet.slot as usize;
+    let mut slot = player.get_mut::<HotbarSlot>()?;
+
+    log::trace!("Got player slot change from {} to {}", slot.get(), new_id);
+
+    slot.set(new_id)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use common::Game;
+    use protocol::packets::client::HeldItemChange;
+
+    use super::*;
+
+    #[test]
+    fn held_item_change() {
+        let mut game = Game::new();
+        let entity = game.ecs.spawn((HotbarSlot::new(0),));
+        let player = game.ecs.entity(entity).unwrap();
+
+        let packet = HeldItemChange { slot: 8 };
+
+        handle_held_item_change(player, packet).unwrap();
+
+        assert_eq!(
+            *game.ecs.get::<HotbarSlot>(entity).unwrap(),
+            HotbarSlot::new(8)
+        );
+    }
 }
