fe6d3886ce51b0323830f55a965c0c8d2de35aeb
Amber Kowalski <amberkowalski03@gmail.com>
2021-03-04 22:40:47 -0600

    Remove the old types.

--- feather/common/src/events.rs
+++ feather/common/src/events.rs
@@ -6,13 +6,8 @@ use parking_lot::RwLock;
 use crate::view::View;
 
 mod block_change;
-mod block_interact;
-mod interact_entity;
 
 pub use block_change::BlockChangeEvent;
-pub use block_interact::{BlockInteractEvent, BlockPlacementEvent};
-pub use interact_entity::InteractEntityEvent;
-
 /// Triggered when a player joins the `Game`.
 #[derive(Debug)]
 pub struct PlayerJoinEvent;
