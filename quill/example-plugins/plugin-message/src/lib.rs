744e85e281913ad270eda23c210242607530e975
Derek Lee <derek.evilcornrules@gmail.com>
2021-03-06 07:05:15 -0800

    Resolve Clippy warning

--- quill/example-plugins/plugin-message/src/lib.rs
+++ quill/example-plugins/plugin-message/src/lib.rs
@@ -17,16 +17,13 @@ impl Plugin for PluginMessage {
 
 fn plugin_message_system(_plugin: &mut PluginMessage, game: &mut Game) {
     for (entity, (_, position)) in game.query::<(&Player, &Position)>() {
-        match position.block() {
-            BlockPosition { x: 10..=12, y: _, z: 10..=12 } => {
-                let mut data = Vec::new();
-                data.extend_from_slice(&u16::to_be_bytes(7));
-                data.extend_from_slice(b"Connect");
-                data.extend_from_slice(&u16::to_be_bytes(5));
-                data.extend_from_slice(b"lobby");
-                Game::send_plugin_message(entity.id(), "bungeecord:main", &data);
-            },
-            _ => {},
+        if let BlockPosition { x: 10..=12, y: _, z: 10..=12 } = position.block() {
+            let mut data = Vec::new();
+            data.extend_from_slice(&u16::to_be_bytes(7));
+            data.extend_from_slice(b"Connect");
+            data.extend_from_slice(&u16::to_be_bytes(5));
+            data.extend_from_slice(b"lobby");
+            Game::send_plugin_message(entity.id(), "bungeecord:main", &data);
         }
     }
 }
