0cf017c9ad9e0da2693c8d1872da774402cf467a
Miro-Andrin <miro.sveits@gmail.com>
2021-05-16 10:30:32 +0200

    Player abilities packet (#425)

---
+++ quill/common/src/events/change.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CreativeFlyingEvent {
+    pub is_flying: bool,
+}
+
+impl CreativeFlyingEvent {
+    pub fn new(changed_to: bool) -> Self {
+        Self {
+            is_flying: changed_to,
+        }
+    }
+}
