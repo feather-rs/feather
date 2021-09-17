c1860701f80f0c21d6e7927c94ab139192a22c5f
Iaiao <err266228@gmail.com>
2021-09-17 19:02:04 +0300

    Fix compilation on latest nightly

--- feather/worldgen/src/superflat.rs
+++ feather/worldgen/src/superflat.rs
@@ -22,9 +22,8 @@ impl WorldGenerator for SuperflatWorldGenerator {
             if layer.height == 0 {
                 continue;
             }
-            // FIXME: get rid of this hack by having a constistent naming convention - Item::name() returns `stone` but BlockId::from_identifier requires `minecraft:stone`
-            let layer_block =
-                BlockId::from_identifier(("minecraft:".to_owned() + &layer.block).as_str());
+            // FIXME: get rid of this hack by having a consistent naming convention - Item::name() returns `stone` but BlockId::from_identifier requires `minecraft:stone`
+            let layer_block = BlockId::from_identifier(&format!("minecraft:{}", layer.block));
             if let Some(layer_block) = layer_block {
                 for y in y_counter..(y_counter + layer.height) {
                     for x in 0..16 {
