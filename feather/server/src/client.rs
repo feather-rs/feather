cf650cc0b1ec1da717ee69477e6d44fb6f97f697
David Alasow <davidalasow@gmail.com>
2021-09-24 22:02:44 +0200

    Revert "Datapack parsing (#455)" (#478)
    * Revert "Datapack parsing (#455)"
    
    This reverts commit 7e46ec7f5954a5a16f9426efdc1ee16c8cd2de2e.
    
    * removed unused variable.

--- feather/server/src/client.rs
+++ feather/server/src/client.rs
@@ -188,12 +188,7 @@ impl Client {
         self.network_id = Some(network_id);
     }
 
-    pub fn send_join_game(
-        &self,
-        gamemode: Gamemode,
-        previous_gamemode: PreviousGamemode,
-        game: &common::Game,
-    ) {
+    pub fn send_join_game(&self, gamemode: Gamemode, previous_gamemode: PreviousGamemode) {
         log::trace!("Sending Join Game to {}", self.username);
         // Use the dimension codec sent by the default vanilla server. (Data acquired via tools/proxy)
         let dimension_codec = nbt::Blob::from_reader(&mut Cursor::new(include_bytes!(
@@ -222,8 +217,6 @@ impl Client {
             is_debug: false,
             is_flat: false,
         });
-
-        self.send_packet(game.tag_registry.all_tags());
     }
 
     pub fn send_brand(&self) {
