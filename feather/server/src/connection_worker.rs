8afac026e8457101b3831dc19050e1f345fbf6af
YuJin Kim <yujinkim@mines.edu>
2021-10-12 12:17:15 -0600

    Succinctly print panic in connection_worker.rs at TRACE level
    Just in case a Tokio task panics, it prints the reason why it panicked. No trace, though.

--- feather/server/src/connection_worker.rs
+++ feather/server/src/connection_worker.rs
@@ -145,6 +145,7 @@ impl Worker {
         tokio::task::spawn(async move {
             let result = reader.race(writer).await.expect("task panicked");
             if let Err(e) = result {
+                log::trace!("Panic error is: {}", e);
                 let message = disconnected_message(e);
                 log::debug!("{} lost connection: {}", username, message);
             }
