fdc8f22d0b3309e4bf65dab9869a4469e0bf7c1a
caelunshun <caelunshun@gmail.com>
2022-04-23 16:06:35 -0600

    Port SupportType and PlacementType to libcraft-blocks

--- libcraft/blocks/src/lib.rs
+++ libcraft/blocks/src/lib.rs
@@ -4,10 +4,12 @@ pub mod data;
 mod registry;
 mod simplified_block;
 mod utils;
+mod metadata;
 
 pub use block::BlockKind;
 pub use block_data::BlockData;
 pub use registry::BlockState;
 pub use simplified_block::SimplifiedBlockKind;
+pub use metadata::{PlacementType, SupportType};
 
 pub const HIGHEST_ID: u16 = 20341;
